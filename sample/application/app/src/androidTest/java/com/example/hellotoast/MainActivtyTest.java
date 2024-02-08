857cb4ee3f6cdf2b3595ca5dedacb95c134b0182
achrafbenabdalla <125751507+achrafbenabdalla@users.noreply.github.com>
2024-02-08 20:54:18 +0100

    test3

--- sample/application/app/src/androidTest/java/com/example/hellotoast/MainActivtyTest.java
+++ sample/application/app/src/androidTest/java/com/example/hellotoast/MainActivtyTest.java
@@ -1,14 +1,13 @@
 package com.example.hellotoast;
 
-import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.example.android.hellotoast.MainActivity;
-import com.example.android.hellotoast.NewActivity;
 import com.example.android.hellotoast.R;
 
 import static androidx.test.espresso.action.ViewActions.click;
@@ -16,14 +15,8 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import android.content.Intent;
-
 @RunWith(AndroidJUnit4.class)
-public class instrumentedtest {
-
-
-
-
+public class MainActivtyTest {
 
     @Test
     public void testSayHelloButton() {
@@ -40,23 +33,13 @@ public class instrumentedtest {
         // Launch the MainActivity
         ActivityScenario.launch(MainActivity.class);
 
-        // Click on the Say Hello button
+        // Click on the count button
+        Espresso.onView(ViewMatchers.withId(R.id.button_count)).perform(click());
+        // Click on the say hello button
         Espresso.onView(ViewMatchers.withId(R.id.button_toast)).perform(click());
 
-        // Get the count from the Intent
-        Intent intent = getActivityIntent();
-        int count = intent.getIntExtra(MainActivity.COUNT_KEY, 0);
+        // Wait for NewActivity to appear
+        Espresso.onView(withId(R.id.show_main_count)).check(matches(withText("1")));
 
-        // Launch the NewActivity
-        ActivityScenario.launch(NewActivity.class);
-
-        // Check if the TextView in NewActivity displays the correct count
-        Espresso.onView(withId(R.id.show_main_count)).check(matches(withText(String.valueOf(count))));
-    }
-
-    private Intent getActivityIntent() {
-        Intent intent = new Intent();
-        intent.putExtra(MainActivity.COUNT_KEY, 0); // Replace with the actual count value you expect from MainActivity
-        return intent;
     }
 }
